cdd8360644edd40cef3fd069619576e708835426
梦靡 <LkeL@gmail.com>
2017-06-25 18:52:47 +0800

    demo更新

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -4,6 +4,7 @@ import * as className from './style/demo.less';
  * This module will be hot-reloaded and rendered upon modification
  */
 console.log(className);
+
 export class Greeting extends React.Component<{ name: String }, {}> {
     render() {
         return <h1 className={className.hello}>Greeting! this demo create by {this.props.name}</h1>;
